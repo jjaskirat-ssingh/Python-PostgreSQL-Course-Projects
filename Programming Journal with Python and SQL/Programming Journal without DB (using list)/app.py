d3530372482b2c8ca13f3fffefd30c67fc368e93
Jaskirat Singh <72308994+jjaskirat-ssingh@users.noreply.github.com>
2021-09-27 00:03:09 +0530

    Programming Journal using List

---
+++ Programming Journal with Python and SQL/Programming Journal without DB (using list)/app.py
@@ -0,0 +1,32 @@
+from database import add_entry, get_entries
+
+menu = """Please select one of the following options:
+1) Add new entry 
+2) View entries
+3) Exit
+
+Your selection: """ 
+
+welcome = "Welcome to the programming diary!"
+
+def prompt_new_entry():
+    entry_content = input("What have you learned today? ")
+    entry_date = input("Enter the date: ")
+    add_entry(entry_content, entry_date)
+
+def view_entries(entries):
+    for entry in entries:
+        print(f"{entry['date']}\n{entry['content']}\n\n")
+
+print(welcome)
+
+while(user_input := int(input(menu))) != 3:   # input taken using walrus operator :=
+    if user_input == 1:
+        prompt_new_entry()
+    elif user_input == 2:
+        # view_entries(get_entries())
+        entries = get_entries()
+        view_entries(entries)
+    else:
+        print("Invalid option, please try again!")
+    
---
+++ Programming Journal with Python and SQL/Programming Journal without DB (using list)/database.py
@@ -0,0 +1,7 @@
+entries = []
+
+def add_entry(entry_content, entry_date):
+    entries.append({"content": entry_content, "date": entry_date})
+
+def get_entries():
+    return entries
