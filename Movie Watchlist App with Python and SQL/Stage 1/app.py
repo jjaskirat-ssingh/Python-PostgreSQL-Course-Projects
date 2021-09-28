b63d556371d3c1c61be5367e21d7948b448358a0
Jaskirat Singh <72308994+jjaskirat-ssingh@users.noreply.github.com>
2021-09-28 18:00:17 +0530

    Stage 1 of Movie Watchlist App

---
+++ Movie Watchlist App with Python and SQL/Stage 1/app.py
@@ -0,0 +1,64 @@
+import datetime
+import database
+
+menu = """Please select one of the following options:
+1) Add new movie.
+2) View upcoming movies.
+3) View all movies
+4) Watch a movie
+5) View watched movies.
+6) Delete a movie
+7) Exit.
+
+Your selection: """
+welcome = "Welcome to the watchlist app!"
+
+
+def prompt_add_movie():
+    title = input("Movie title: ")
+    release_date = input("Release date (dd-mm-YYYY): ")
+
+    parsed_date = datetime.datetime.strptime(release_date, "%d-%m-%Y")
+    timestamp = parsed_date.timestamp()
+
+    database.add_movie(title, timestamp)
+
+
+def print_movie_list(heading, movies):
+    print(f"-- {heading} movies --")
+    for movie in movies:
+        movie_date = datetime.datetime.fromtimestamp(movie[1])
+        human_date = movie_date.strftime("%b %d %Y")
+        print(f"{movie[0]} (on {human_date})")
+    print("---- \n")
+
+
+def prompt_watch_movie():
+    movie_title = input("Enter movie title you've watched: ")
+    database.watch_movie(movie_title)
+
+def prompt_delete_movie():
+    movie_title = input("Enter movie title you want to delete: ")
+    database.delete_movie(movie_title)
+
+print(welcome)
+database.create_tables()
+
+while (user_input := int(input(menu))) != 7:
+    if user_input == 1:
+        prompt_add_movie()
+    elif user_input == 2:
+        movies = database.get_movies(True)
+        print_movie_list("Upcoming", movies)
+    elif user_input == 3:
+        movies = database.get_movies()
+        print_movie_list("All", movies)
+    elif user_input == 4:
+        prompt_watch_movie()
+    elif user_input == 5:
+        movies = database.get_watched_movies()
+        print_movie_list("Watched", movies)
+    elif user_input == 6:
+        prompt_delete_movie()
+    else:
+        print("Invalid input, please try again!")
---
+++ Movie Watchlist App with Python and SQL/Stage 1/database.py
@@ -0,0 +1,49 @@
+import sqlite3
+import datetime
+
+CREATE_MOVIES_TABLE = """CREATE TABLE IF NOT EXISTS movies (
+    title TEXT,
+    release_timestamp REAL,
+    watched INTEGER
+);"""
+
+INSERT_MOVIES = "INSERT INTO movies (title, release_timestamp, watched) VALUES (?, ?, 0);"
+SELECT_ALL_MOVIES = "SELECT * FROM movies"
+SELECT_UPCOMING_MOVIES = "SELECT * FROM movies WHERE release_timestamp > ?;"
+SELECT_WATCHED_MOVIES = "SELECT * FROM movies WHERE watched = 1;"
+SET_MOVIE_WATCHED = "UPDATE movies SET watched = 1 WHERE title = ?;"
+DELETE_MOVIE = "DELETE FROM movies WHERE title = ?;"
+
+connection = sqlite3.connect("data.db")
+
+def create_tables():
+    with connection:
+        connection.execute(CREATE_MOVIES_TABLE)
+
+def add_movie(title, release_timestamp):
+    with connection:
+        connection.execute(INSERT_MOVIES, (title, release_timestamp))
+
+def get_movies(upcoming = False):
+    with connection:
+        cursor = connection.cursor()
+        if upcoming:
+            today_timestamp = datetime.datetime.today().timestamp()
+            cursor.execute(SELECT_UPCOMING_MOVIES, (today_timestamp, ))
+        else:
+            cursor.execute(SELECT_ALL_MOVIES)
+        return cursor.fetchall()
+
+def watch_movie(title):
+    with connection:
+        connection.execute(SET_MOVIE_WATCHED, (title, ))
+
+def get_watched_movies():
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(SELECT_WATCHED_MOVIES)
+        return cursor.fetchall()
+
+def delete_movie(title):
+    with connection:
+        connection.execute(DELETE_MOVIE, (title, ))
